46b79b9e106c10d554ace79c2e55c54f13e33951
soheila <soheilalotfi@ymail.com>
2019-10-11 17:12:28 -0400

    add zootopia and images.json

--- clicky-game/src/Components/Game.js
+++ clicky-game/src/Components/Game.js
@@ -1,26 +1,56 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Image from "./Image";
-
-class Game extends Component {
-
-    state = {
-        score: 0,
-        topScore: 0,
-        guessed: "Click an image to begin"
+import Images from "../images.json";
+const styles={
+    container:{
+    maxWidth: "960px",
+    border: "solid 5px black",
+    margin:"auto",
+    height:"800px",
+    marginTop:"100px",
+    background: "yellow"
     }
-
-    render() {
-
-        return (
-            <>
-                <Header guessed={this.state.guessed} score={this.state.score} topScore={this.state.topScore} />
-                <Image />
-
-            </>
-        );
-    }
-
-
 }
+
+        class Game extends Component {
+         
+
+            state = {
+            score: 0,
+            topScore: 0,
+            guessed: "Click an image to begin",
+            updateImages :Images
+            };
+
+        
+              
+
+            };
+
+            render() {
+
+                return (
+                    <>
+                    <Header guessed={this.state.guessed} score={this.state.score} topScore={this.state.topScore} />
+                    <div style={styles.container}>
+                    {Images.map(img => (
+                        <Image
+                            handleClickImage={this.handleClickImage}
+                            id={img.id}
+                            key={img.id}
+                            src={img.src}
+
+                        />
+                    ))}
+                    </div>
+                  
+                
+                    </>
+                    );
+            }
+
+
+        }
+
 export default Game;
---
+++ clicky-game/src/Components/Image.js
@@ -0,0 +1,34 @@
+import React from "react";
+// import images from "../images.json"
+
+const styles={
+        container:{
+        maxWidth: "960px",
+        border: "solid 5px black",
+        margin:"auto",
+        height:"800px",
+        marginTop:"100px",
+        background: "yellow"
+        },
+
+        img:{
+        width :"200px",
+        height: "200px",
+        margin: "10px"
+        }
+
+};
+
+
+function Image(props) {
+
+
+    return (
+           
+         
+    )
+
+
+}
+
+export default Image;
